fab1ea21dfc0d5cf6ba4239d40007d5e9607c235
Felix <jonfelixrico@gmail.com>
2022-10-21 00:57:23 +0800

    Integate session

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as session from 'express-session'
+import { randomUUID } from 'crypto'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(
+    session({
+      secret: randomUUID(),
+      resave: false,
+      saveUninitialized: false,
+    }),
+  )
+
   await app.listen(9085)
 }
 bootstrap()
