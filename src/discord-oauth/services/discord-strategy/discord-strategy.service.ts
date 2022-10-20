2fc5961478d8b192ec01a28c819a2763c384ad2f
Felix <jonfelixrico@gmail.com>
2022-10-20 23:22:38 +0800

    Integrate oauth2 instead of discord

--- src/discord-oauth/services/discord-strategy/discord-strategy.service.ts
+++ src/discord-oauth/services/discord-strategy/discord-strategy.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
-import { Strategy, StrategyOptions } from 'passport-discord'
+import { Strategy, StrategyOptions } from 'passport-oauth2'
 
 @Injectable()
 export class DiscordStrategyService extends PassportStrategy(Strategy) {
   constructor(cfg: ConfigService) {
     super({
+      authorizationURL: cfg.getOrThrow('DISCORD_AUTHORIZATION_URL'),
+      tokenURL: cfg.getOrThrow('DISCORD_TOKEN_URL'),
+
       clientID: cfg.getOrThrow('DISCORD_OAUTH_CLIENT_ID'),
       clientSecret: cfg.getOrThrow('DISCORD_OAUTH_CLIENT_SECRET'),
       callbackURL: cfg.getOrThrow('DISCORD_OAUTH_CALLBACK_URL'),
+
       scope: cfg
         .getOrThrow<string>('DISCORD_OAUTH_SCOPE')
         .split(',')
