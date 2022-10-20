794b35e904a4d9d435f6b735416bd9a35786b382
Felix <jonfelixrico@gmail.com>
2022-10-20 22:25:54 +0800

    Create the discord strategy service

--- src/discord-oauth/discord-oauth.module.ts
+++ src/discord-oauth/discord-oauth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './controllers/auth/auth.controller'
+import { DiscordStrategyService } from './discord-strategy/discord-strategy.service'
 
 @Module({
   controllers: [AuthController],
+  providers: [DiscordStrategyService],
 })
 export class DiscordOauthModule {}
