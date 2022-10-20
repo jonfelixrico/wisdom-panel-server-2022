4b83f2dc1c3c2bb284a230acba7aaf9215ecb706
Felix <jonfelixrico@gmail.com>
2022-10-20 23:27:19 +0800

    Adjust oauth routes

--- src/discord-oauth/controllers/auth/auth.controller.ts
+++ src/discord-oauth/controllers/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @UseGuards(AuthGuard('oauth2'))
+  @Get()
+  async startOAuth() {
+    return
+  }
+
+  @UseGuards(AuthGuard('oauth2'))
+  @Get('callback')
+  async oAuthCallback() {
+    return
+  }
+}
