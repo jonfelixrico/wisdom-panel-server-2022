63f06b33955dd07279a1406a052cbb810c36c1f5
Felix <jonfelixrico@gmail.com>
2022-10-21 01:29:03 +0800

    Integrate PublicRoute to DiscordAuth

--- src/discord-oauth/controllers/auth/auth.controller.ts
+++ src/discord-oauth/controllers/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Controller, Get, Req, Res, Session, UseGuards } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { AuthGuard } from '@nestjs/passport'
 import { Request, Response } from 'express'
+import { PublicRoute } from 'src/guards/public-route.decorator'
 
 @Controller('auth')
 export class AuthController {
   constructor(private cfg: ConfigService) {}
 
+  @PublicRoute()
   @UseGuards(AuthGuard('oauth2'))
   @Get()
   async startOAuth() {
     return
   }
 
+  @PublicRoute()
   @UseGuards(AuthGuard('oauth2'))
   @Get('callback')
   async oAuthCallback(
