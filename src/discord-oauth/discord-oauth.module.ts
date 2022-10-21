5099866d5f622bf901abd258ba0cb4bde9c84189
Felix <jonfelixrico@gmail.com>
2022-10-21 21:24:17 +0800

    Push the relocate code

--- src/discord-oauth/discord-oauth.module.ts
+++ src/discord-oauth/discord-oauth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './controllers/auth/auth.controller'
 import { DiscordStrategyService } from './services/discord-strategy/discord-strategy.service'
-import { OauthHelperService } from './oauth-helper/oauth-helper.service'
+import { OauthHelperService } from './services/oauth-helper/oauth-helper.service'
 
 @Module({
   controllers: [AuthController],
