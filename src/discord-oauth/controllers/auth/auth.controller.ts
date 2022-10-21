8c1ce7d022bc5c0cf74e462a38da7f73e45be6df
Felix <jonfelixrico@gmail.com>
2022-10-21 22:13:09 +0800

    Implement POST /auth

--- src/discord-oauth/controllers/auth/auth.controller.ts
+++ src/discord-oauth/controllers/auth/auth.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Req, Res } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req, Res, Session } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { Request, Response } from 'express'
 import { stringify } from 'qs'
 import { OAuthHelperService } from 'src/discord-oauth/services/oauth-helper/oauth-helper.service'
 import { PublicRoute } from 'src/guards/public-route.decorator'
+
+interface CodePayload {
+  code: string
+}
 
 @Controller('auth')
 export class AuthController {
@@ -24,7 +28,7 @@ export class AuthController {
 
   @PublicRoute()
   @Get('callback')
-  async oAuthCallback(@Res() res: Response, @Req() req: Request) {
+  async oauthCallback(@Res() res: Response, @Req() req: Request) {
     const url = new URL(this.cfg.getOrThrow('URL'))
 
     const sp = url.searchParams
@@ -33,5 +37,16 @@ export class AuthController {
     }
 
     res.redirect(url.toString())
+  }
+
+  @PublicRoute()
+  @Post()
+  async exchangeAccessCode(
+    @Body() { code }: CodePayload,
+    @Session() session: Record<string, unknown>,
+  ) {
+    const authToken = await this.helper.exchangeAccessCode(code)
+    session.isAuthenticated = true
+    session.tokens = authToken
   }
 }
