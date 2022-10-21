8f9a2a97e4a16242761faea8b104dda3744aba27
Felix <jonfelixrico@gmail.com>
2022-10-22 02:06:51 +0800

    Create endpoint for getting the discord user data

--- src/discord-data/controllers/user/user.controller.ts
+++ src/discord-data/controllers/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
+import { DiscordRest } from 'src/param-decorators/discord-rest.decorator'
+import { RESTGetAPICurrentUserResult, Routes } from 'discord-api-types/v10'
+import { AxiosInstance } from 'axios'
 
 @Controller('discord/user')
-export class UserController {}
+export class UserController {
+  @Get('/@me')
+  async getSessionUserData(@DiscordRest() client: AxiosInstance) {
+    const { data } = await client.get<RESTGetAPICurrentUserResult>(
+      Routes.user(),
+    )
+
+    return data
+  }
+}
