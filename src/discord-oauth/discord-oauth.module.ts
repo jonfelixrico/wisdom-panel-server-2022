e710f2729fcb0dd62d3ebf3ce2f7f9226c12f9a2
Felix <jonfelixrico@gmail.com>
2022-10-21 23:58:12 +0800

    Remove passport

--- src/discord-oauth/discord-oauth.module.ts
+++ src/discord-oauth/discord-oauth.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common'
-import { PassportModule } from '@nestjs/passport'
 import { HttpModule } from 'nestjs-http-promise'
 import { AuthController } from './controllers/auth/auth.controller'
-import { DiscordStrategyService } from './services/discord-strategy/discord-strategy.service'
 import { OAuthHelperService } from './services/oauth-helper/oauth-helper.service'
 
 @Module({
   controllers: [AuthController],
-  providers: [DiscordStrategyService, OAuthHelperService],
-  imports: [
-    PassportModule.register({
-      session: true,
-    }),
-    HttpModule,
-  ],
+  providers: [OAuthHelperService],
+  imports: [HttpModule],
 })
 export class DiscordOauthModule {}
