ea81da60a1220a4f9d1fb2f2ad330fb4dc8f00c9
Felix <jonfelixrico@gmail.com>
2022-10-21 21:38:42 +0800

    Add HttpModule as an import

--- src/discord-oauth/discord-oauth.module.ts
+++ src/discord-oauth/discord-oauth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
+import { HttpModule } from 'nestjs-http-promise'
 import { AuthController } from './controllers/auth/auth.controller'
 import { DiscordStrategyService } from './services/discord-strategy/discord-strategy.service'
 import { OAuthHelperService } from './services/oauth-helper/oauth-helper.service'
@@ -11,6 +12,7 @@ import { OAuthHelperService } from './services/oauth-helper/oauth-helper.service
     PassportModule.register({
       session: true,
     }),
+    HttpModule,
   ],
 })
 export class DiscordOauthModule {}
