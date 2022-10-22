cc853a03bbc4265251e80c087197158142403ce7
Felix <jonfelixrico@gmail.com>
2022-10-22 22:33:11 +0800

    Create Axios client for the discord bot

--- src/discord-data/discord-data.module.ts
+++ src/discord-data/discord-data.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common'
 import { UserController } from './controllers/user/user.controller'
 import { ServerController } from './controllers/server/server.controller'
+import { DiscordBotApiClient } from './discord-bot-api-client.class'
+import { ConfigService } from '@nestjs/config'
+import axios from 'axios'
+import { RouteBases } from 'discord-api-types/v10'
 
 @Module({
+  providers: [
+    {
+      provide: DiscordBotApiClient,
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) =>
+        axios.create({
+          baseURL: RouteBases.api,
+          headers: {
+            Authorization: `Bearer ${cfg.getOrThrow('DISCORD_BOT_TOKEN')}`,
+          },
+        }),
+    },
+  ],
   controllers: [UserController, ServerController],
 })
 export class DiscordDataModule {}
