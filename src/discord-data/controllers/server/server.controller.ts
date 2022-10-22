f103a2fbc9dacb14c0c7cb15c1d9dc600d216a7b
Felix <jonfelixrico@gmail.com>
2022-10-22 19:17:07 +0800

    Implement endpoints for getting guild data

--- src/discord-data/controllers/server/server.controller.ts
+++ src/discord-data/controllers/server/server.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
+import { AxiosInstance } from 'axios'
+import {
+  RESTGetAPIGuildMembersResult,
+  RESTGetAPIGuildResult,
+  Routes,
+} from 'discord-api-types/v10'
+import { DiscordApi } from 'src/param-decorators/discord-api.decorator'
 
-@Controller('discord/server')
-export class ServerController {}
+@Controller('discord/server/:serverId')
+export class ServerController {
+  @Get()
+  async getServer(@DiscordApi() api: AxiosInstance, @Param() serverId: string) {
+    const { data } = await api.get<RESTGetAPIGuildResult>(
+      Routes.guild(serverId),
+    )
+    return data
+  }
+
+  @Get()
+  async listServerMembers(
+    @DiscordApi() api: AxiosInstance,
+    @Param() serverId: string,
+  ) {
+    const { data } = await api.get<RESTGetAPIGuildMembersResult>(
+      Routes.guildMembers(serverId),
+    )
+    return data
+  }
+}
