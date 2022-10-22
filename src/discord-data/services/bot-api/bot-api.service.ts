ea92c4310f7a0e425db19a5fc18fec6094dda50a
Felix <jonfelixrico@gmail.com>
2022-10-22 22:42:20 +0800

    Implement some methods in the BotApiService

--- src/discord-data/services/bot-api/bot-api.service.ts
+++ src/discord-data/services/bot-api/bot-api.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common'
+import {
+  RESTGetAPICurrentUserGuildsResult,
+  RESTGetAPIGuildMemberResult,
+  Routes,
+} from 'discord-api-types/v10'
+import { DiscordBotApiClient } from 'src/discord-data/discord-bot-api-client.class'
 
+/**
+ * Uses the Wisdom bot user to perform some API calls.
+ * This was necessary because some routes are necessary for the user.
+ */
 @Injectable()
-export class BotApiService {}
+export class BotApiService {
+  constructor(private api: DiscordBotApiClient) {}
+
+  /**
+   * Lists down the servers that the Wisdom bot is a mebmer of.
+   * @returns
+   */
+  async getServers() {
+    const { data } = await this.api.get<RESTGetAPICurrentUserGuildsResult>(
+      Routes.userGuilds(),
+    )
+
+    return data
+  }
+
+  /**
+   * Lists down the members of a server.
+   * Take note that we should expect this to throw an error if we tried to access information for a server
+   * that the bot is not a part of.
+   *
+   * @param serverId
+   * @returns
+   */
+  async getServerMembers(serverId: string) {
+    const { data } = await this.api.get<RESTGetAPIGuildMemberResult>(
+      Routes.guildMembers(serverId),
+    )
+
+    return data
+  }
+}
