6999482e6729aae6e4522a8c9d081f2cc276d69c
Felix <jonfelixrico@gmail.com>
2023-01-02 23:07:30 +0800

    Implement fetching of members

--- src/discord-api/services/server-member-repository/server-member-repository.service.ts
+++ src/discord-api/services/server-member-repository/server-member-repository.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { DiscordBotApi } from 'src/discord-api/providers/discord-bot-api.provider'
+import { DISCORD_BOT_CACHE } from 'src/discord-api/providers/discord-bot-cache.provider'
+import { Cache } from 'cache-manager'
+import { RESTGetAPIGuildMemberResult, Routes } from 'discord-api-types/v10'
 
 @Injectable()
 export class ServerMemberRepositoryService {
-  constructor(private botApi: DiscordBotApi) {}
+  constructor(
+    private api: DiscordBotApi,
+    @Inject(DISCORD_BOT_CACHE) private cache: Cache,
+  ) {}
+
+  async getMember(serverId: string, userId: string) {
+    return this.cache.wrap(
+      `server/${serverId}/user/${userId}`,
+      async () =>
+        await this.api.get<RESTGetAPIGuildMemberResult>(
+          Routes.guildMember(serverId, userId),
+        ),
+    )
+  }
 }
