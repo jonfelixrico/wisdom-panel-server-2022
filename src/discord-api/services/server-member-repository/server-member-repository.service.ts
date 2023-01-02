0b03e69e23be0b7e9c3d0194d6b105eee4dc51d4
Felix <jonfelixrico@gmail.com>
2023-01-02 22:26:35 +0800

    Apply minor changes

--- src/discord-api/services/server-member-repository/server-member-repository.service.ts
+++ src/discord-api/services/server-member-repository/server-member-repository.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common'
+import { DiscordBotApiClient } from 'src/discord-api/providers/discord-bot-api/discord-bot-api-client.class'
 
 @Injectable()
-export class ServerMemberRepositoryService {}
+export class ServerMemberRepositoryService {
+  constructor(private botApi: DiscordBotApiClient) {}
+}
