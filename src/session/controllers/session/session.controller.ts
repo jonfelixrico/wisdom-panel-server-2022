5c141fe41811cd04ccd42113af5e921448145549
Felix <jonfelixrico@gmail.com>
2023-01-02 13:49:18 +0800

    Add logging

--- src/session/controllers/session/session.controller.ts
+++ src/session/controllers/session/session.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Delete, Get, Req } from '@nestjs/common'
+import { Controller, Delete, Get, Logger, Req } from '@nestjs/common'
 import { Request } from 'express'
 
 @Controller('session')
 export class SessionController {
+  private readonly LOGGER = new Logger(SessionController.name)
+
   /**
    * Sole purpose is for session-checking.
    * @returns
@@ -17,11 +19,22 @@ export class SessionController {
 
   @Delete()
   async destroySession(@Req() req: Request) {
+    const { LOGGER } = this
+
+    // TODO include user id here
+    LOGGER.debug('Logout attempted by user')
     await new Promise<void>((resolve, reject) => {
-      req.session.destroy((err) => {
+      req.session.destroy((err: Error) => {
         if (err) {
           reject(err)
+          // TODO include user id here
+          LOGGER.error(
+            `Error encountered while logging out: ${err.message}`,
+            err.stack,
+          )
         } else {
+          // TODO include user id here
+          LOGGER.verbose('User has logged out')
           resolve()
         }
       })
