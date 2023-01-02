1650339cc99cd1a2f90eed211a5a660cd8ededc2
Felix <jonfelixrico@gmail.com>
2023-01-02 13:33:52 +0800

    Create session destroy endpoints

--- src/session/controllers/session/session.controller.ts
+++ src/session/controllers/session/session.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Delete, Get, Req } from '@nestjs/common'
+import { Request } from 'express'
 
 @Controller('session')
 export class SessionController {
+  /**
+   * Sole purpose is for session-checking.
+   * @returns
+   */
   @Get()
   async getSession() {
+    /*
+     * Empty since we intend this to just return 200 if a session exists and 401 if otherwise
+     */
     return
+  }
+
+  @Delete()
+  async destroySession(@Req() req: Request) {
+    await new Promise<void>((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      })
+    })
   }
 }
