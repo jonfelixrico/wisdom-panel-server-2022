d1befed68a6a743667469311d45e79e3d9783039
Felix <jonfelixrico@gmail.com>
2023-01-02 23:34:21 +0800

    Add APITag

--- src/discord-api/controllers/server-member/server-member.controller.ts
+++ src/discord-api/controllers/server-member/server-member.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 import { ServerMemberRepositoryService } from 'src/discord-api/services/server-member-repository/server-member-repository.service'
 import { getMemberAvatarUrl } from 'src/discord-api/utils/avatar.utils'
 import { ServerMemberDto } from './server-member.dto'
 
+@ApiTags('discord')
 @Controller('server/:serverId/user')
 export class ServerMemberController {
   constructor(private repo: ServerMemberRepositoryService) {}
