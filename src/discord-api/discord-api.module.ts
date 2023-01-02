dc56897415f2fb0b4ab2eb13dca8fd48412f87d9
Felix <jonfelixrico@gmail.com>
2023-01-02 19:19:58 +0800

    Create service for getting server member data

--- src/discord-api/discord-api.module.ts
+++ src/discord-api/discord-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DISCORD_BOT_API_PROVIDER } from './providers/discord-bot-api'
 import { DiscordBotApiClient } from './providers/discord-bot-api/discord-bot-api-client.class'
+import { ServerMemberRepositoryService } from './services/server-member-repository/server-member-repository.service'
 
 @Module({
-  providers: [DISCORD_BOT_API_PROVIDER],
-  exports: [DiscordBotApiClient],
+  providers: [DISCORD_BOT_API_PROVIDER, ServerMemberRepositoryService],
+  exports: [DiscordBotApiClient, ServerMemberRepositoryService],
 })
 export class DiscordApiModule {}
