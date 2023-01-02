786c574ecc6d5ba2e74cec875269e55f493db667
Felix <jonfelixrico@gmail.com>
2023-01-02 19:21:58 +0800

    Remove the bot api as an export

--- src/discord-api/discord-api.module.ts
+++ src/discord-api/discord-api.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
 import { DISCORD_BOT_API_PROVIDER } from './providers/discord-bot-api'
-import { DiscordBotApiClient } from './providers/discord-bot-api/discord-bot-api-client.class'
 import { ServerMemberRepositoryService } from './services/server-member-repository/server-member-repository.service'
 
 @Module({
   providers: [DISCORD_BOT_API_PROVIDER, ServerMemberRepositoryService],
-  exports: [DiscordBotApiClient, ServerMemberRepositoryService],
+  exports: [ServerMemberRepositoryService],
 })
 export class DiscordApiModule {}
