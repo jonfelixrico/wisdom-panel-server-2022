b4e51e4b34cd3d86251e1964939af9614cd816f3
Felix <jonfelixrico@gmail.com>
2023-01-02 23:43:32 +0800

    Adjust the utils to support GIF

--- src/discord-api/utils/avatar.utils.ts
+++ src/discord-api/utils/avatar.utils.ts
@@ -4,19 +4,31 @@ import {
   CDNRoutes,
   DefaultUserAvatarAssets,
   ImageFormat,
+  RouteBases,
 } from 'discord-api-types/v10'
+
+function getFormat(hash: string) {
+  if (hash.startsWith('a_')) {
+    return ImageFormat.GIF
+  }
+
+  return ImageFormat.PNG
+}
 
 export function getMemberAvatarUrl(
   serverId: string,
   member: APIGuildMember,
 ): string {
   if (member.avatar) {
-    return CDNRoutes.guildMemberAvatar(
-      serverId,
-      member.user.id,
-      member.avatar,
-      ImageFormat.WebP,
-    )
+    return new URL(
+      CDNRoutes.guildMemberAvatar(
+        serverId,
+        member.user.id,
+        member.avatar,
+        getFormat(member.avatar),
+      ),
+      RouteBases.cdn,
+    ).toString()
   }
 
   return getUserAvatarUrl(member.user)
@@ -25,11 +37,17 @@ export function getMemberAvatarUrl(
 export function getUserAvatarUrl(user: APIUser): string {
   if (user.avatar) {
     // user has an avatar
-    return CDNRoutes.userAvatar(user.id, user.avatar, ImageFormat.WebP)
+    return new URL(
+      CDNRoutes.userAvatar(user.id, user.avatar, getFormat(user.avatar)),
+      RouteBases.cdn,
+    ).toString()
   }
 
   // user has no avatar
-  return CDNRoutes.defaultUserAvatar(
-    (Number(user.discriminator) % 5) as DefaultUserAvatarAssets,
-  )
+  return new URL(
+    CDNRoutes.defaultUserAvatar(
+      (Number(user.discriminator) % 5) as DefaultUserAvatarAssets,
+    ),
+    RouteBases.cdn,
+  ).toString()
 }
