5e759fcbdeea306b028cd0ef4e8e97019cb924de
Felix <jonfelixrico@gmail.com>
2023-01-02 23:33:56 +0800

    Use the utils

--- src/discord-api/controllers/server-member/server-member.controller.ts
+++ src/discord-api/controllers/server-member/server-member.controller.ts
@@ -1,10 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common'
-import {
-  CDNRoutes,
-  ImageFormat,
-  DefaultUserAvatarAssets,
-} from 'discord-api-types/v10'
 import { ServerMemberRepositoryService } from 'src/discord-api/services/server-member-repository/server-member-repository.service'
+import { getMemberAvatarUrl } from 'src/discord-api/utils/avatar.utils'
 import { ServerMemberDto } from './server-member.dto'
 
 @Controller('server/:serverId/user')
@@ -16,28 +12,11 @@ export class ServerMemberController {
     @Param('userId') userId: string,
     @Param('serverId') serverId: string,
   ): Promise<ServerMemberDto> {
-    const { avatar, nick, user } = await this.repo.getMember(serverId, userId)
-    const username = nick || user.username
+    const member = await this.repo.getMember(serverId, userId)
 
-    let avatarUrl: string
-    if (avatar) {
-      // get server avatar first
-      avatarUrl = CDNRoutes.guildMemberAvatar(
-        serverId,
-        userId,
-        avatar,
-        ImageFormat.WebP,
-      )
-    } else if (user.avatar) {
-      // get user avatar if it doesnt exist
-      avatarUrl = CDNRoutes.userAvatar(userId, user.avatar, ImageFormat.WebP)
-    } else {
-      // get default avatar as fallback
-      avatarUrl = CDNRoutes.defaultUserAvatar(
-        (Number(user.discriminator) % 5) as DefaultUserAvatarAssets,
-      )
-    }
-
-    return new ServerMemberDto(username, avatarUrl)
+    return new ServerMemberDto(
+      member.nick || member.user.username,
+      getMemberAvatarUrl(serverId, member),
+    )
   }
 }
