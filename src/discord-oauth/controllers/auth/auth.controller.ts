6ecbb27134249515550f9109a69d4aa63713ab0e
Felix <jonfelixrico@gmail.com>
2023-01-02 17:36:16 +0800

    Add logic

--- src/discord-oauth/controllers/auth/auth.controller.ts
+++ src/discord-oauth/controllers/auth/auth.controller.ts
@@ -38,15 +38,33 @@ export class AuthController {
   @PublicRoute()
   @Get()
   startOAuth(@Res() res: Response, @Req() req: Request) {
+  startOAuth(@Res() res: Response, @Query('state') state: string) {
     // TODO redirect back to FE if already authenticated
     const query = req.query ?? {}
+    res.redirect(this.oauthHelper.generateAuthorizationUrl(state))
+  }
 
     let stringified: string
     if (Object.keys(query).length) {
       stringified = JSON.stringify(query)
+  private buildRedirectUrl(
+    state?: string,
+    otherParams: Record<string, string> = {},
+  ) {
+    const url = new URL(
+      this.cfg.getOrThrow('DISCORD_OAUTH_FRONTEND_CALLBACK_URL'),
+    )
+
+    for (const key in otherParams) {
+      url.searchParams.set(key, otherParams[key])
+    }
+
+    if (state) {
+      url.searchParams.set('state', state)
     }
 
     res.redirect(this.oauthHelper.generateAuthorizationUrl(stringified))
+    return url.toString()
   }
 
   @ApiOperation({
@@ -76,6 +94,17 @@ export class AuthController {
     const url = new URL(
       this.cfg.getOrThrow('DISCORD_OAUTH_FRONTEND_CALLBACK_URL'),
     )
+  async oauthCallback(
+    @Res() res: Response,
+    @Req() req: Request,
+    @Query() query: Record<string, string>,
+  ) {
+    if (req.session.tokens) {
+      // Handling for already-authenticated users
+      res.redirect(this.buildRedirectUrl())
+    } else if (query.code) {
+      // OAuth was successful
+      const { code, state } = query
 
     const sp = url.searchParams
     for (const key in req.query) {
@@ -86,8 +115,24 @@ export class AuthController {
         sp.append(key, JSON.stringify(value))
       }
     }
+      const authToken = await this.oauthHelper.exchangeAccessCode(code)
+      req.session.tokens = authToken
 
     res.redirect(url.toString())
+      res.redirect(this.buildRedirectUrl(state))
+    } else if (query.error) {
+      // OAuth failed
+      const { error, error_description: errorDescription, state } = query
+      res.redirect(this.buildRedirectUrl(state, { error, errorDescription }))
+    } else {
+      /*
+       * Not authenticated, but no error nor code was present.
+       *
+       * We're not throwing a 400 bad request since we want the front-end to handle the error handling.
+       * Additionally, this is not expected to be called as AJAX.
+       */
+      res.redirect(this.buildRedirectUrl(undefined, { badRequest: 'true' }))
+    }
   }
 
   @ApiOperation({
