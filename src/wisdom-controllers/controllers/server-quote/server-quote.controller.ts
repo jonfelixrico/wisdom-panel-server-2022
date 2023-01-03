ee96d626e7183b3b3a17f41a382ca60a269217a2
Felix <jonfelixrico@gmail.com>
2023-01-04 00:52:10 +0800

    Add ApiOperation docs

--- src/wisdom-controllers/controllers/server-quote/server-quote.controller.ts
+++ src/wisdom-controllers/controllers/server-quote/server-quote.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { QuoteApiService } from 'src/wisdom-api/services/quote-api/quote-api.service'
 import { QuoteDto } from 'src/wisdom-controllers/dto/quote.dto'
 
+@ApiTags('wisdom')
 @Controller('server/:serverId/quote')
 export class ServerQuoteController {
   constructor(private quoteApi: QuoteApiService) {}
 
+  @ApiOperation({
+    operationId: 'getQuote',
+    summary: 'Get a quote from a server',
+  })
   @Get(':quoteId')
   async getQuote(
     // TODO make required
