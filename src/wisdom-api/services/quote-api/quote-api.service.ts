ec79a1433c506e951b4829398a834e9bea62667e
Felix <jonfelixrico@gmail.com>
2023-01-04 00:35:10 +0800

    Add getQuote impl

--- src/wisdom-api/services/quote-api/quote-api.service.ts
+++ src/wisdom-api/services/quote-api/quote-api.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
+import { isAxiosError } from 'axios'
+import { WisdomApiQuoteDto } from 'src/wisdom-api/dto/quote.wisdom-dto'
+import { WisdomApiClient } from 'src/wisdom-api/providers/wisdom-api-client.provider'
 
 @Injectable()
-export class QuoteApiService {}
+export class QuoteApiService {
+  constructor(private api: WisdomApiClient) {}
+
+  async getQuote(serverId: string, quoteId: string) {
+    try {
+      const { data } = await this.api.get<WisdomApiQuoteDto>(
+        `server/${serverId}/quote/${quoteId}`,
+      )
+      return data
+    } catch (e) {
+      if (isAxiosError(e) && e.status === HttpStatus.NOT_FOUND) {
+        return null
+      }
+
+      throw e
+    }
+  }
+}
