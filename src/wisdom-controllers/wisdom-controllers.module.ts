e6792913413edbb7b9a20a708b69eecd661fb80e
Felix <jonfelixrico@gmail.com>
2023-01-04 00:49:53 +0800

    Import the wisdom api

--- src/wisdom-controllers/wisdom-controllers.module.ts
+++ src/wisdom-controllers/wisdom-controllers.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { WisdomApiModule } from 'src/wisdom-api/wisdom-api.module'
 import { ServerQuoteController } from './controllers/server-quote/server-quote.controller'
 
 @Module({
+  imports: [WisdomApiModule],
   controllers: [ServerQuoteController],
 })
 export class WisdomControllersModule {}
