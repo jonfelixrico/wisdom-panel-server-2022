18848541520f47e71e8f6980b9a42fdd99b7af4d
Felix <jonfelixrico@gmail.com>
2023-01-04 00:50:02 +0800

    Create controller impl

--- src/wisdom-controllers/controllers/server-quote/server-quote.controller.ts
+++ src/wisdom-controllers/controllers/server-quote/server-quote.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
+import { QuoteApiService } from 'src/wisdom-api/services/quote-api/quote-api.service'
+import { QuoteDto } from 'src/wisdom-controllers/dto/quote.dto'
 
-@Controller('server-quote')
-export class ServerQuoteController {}
+@Controller('server/:serverId/quote')
+export class ServerQuoteController {
+  constructor(private quoteApi: QuoteApiService) {}
+
+  @Get(':quoteId')
+  async getQuote(
+    // TODO make required
+    @Param('serverId') serverId: string,
+    @Param('quoteId') quoteId: string,
+  ): Promise<QuoteDto> {
+    return await this.quoteApi.getQuote(serverId, quoteId)
+  }
+}
