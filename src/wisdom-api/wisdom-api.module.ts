faac4a05f36b09d851caa074a3d3d903ad140926
Felix <jonfelixrico@gmail.com>
2023-01-03 22:47:40 +0800

    Create the quote API service

--- src/wisdom-api/wisdom-api.module.ts
+++ src/wisdom-api/wisdom-api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { provideWisdomApiClient } from './providers/wisdom-api-client.provider'
+import { QuoteApiService } from './services/quote-api/quote-api.service'
 
 @Module({
-  providers: [provideWisdomApiClient()],
+  providers: [provideWisdomApiClient(), QuoteApiService],
 })
 export class WisdomApiModule {}
