674214f300ba88ba3b1fbd1966f20728ced41021
Felix <jonfelixrico@gmail.com>
2023-01-08 02:10:06 +0800

    Add initial tests

--- test/unit-tests/avatar.util.spec.ts
+++ test/unit-tests/avatar.util.spec.ts
@@ -1,6 +1,32 @@
+import { getMemberAvatarUrl } from 'src/discord-api/utils/avatar.util'
+
 describe('avatar utils', () => {
   describe('getMemberAvatarUrl', () => {
-    test.todo('todo')
+    it('should yield png url if avatar hash is not animated', () => {
+      const url = getMemberAvatarUrl('dummy-server', {
+        avatar: 'member-avatar',
+        user: {
+          avatar: 'user-avatar',
+          discriminator: '1234',
+          id: 'user-id',
+        },
+      })
+
+      expect(url).toContain('png')
+    })
+
+    it('should yield gif url if avatar hash is animated', () => {
+      const url = getMemberAvatarUrl('dummy-server', {
+        avatar: 'a_member-avatar',
+        user: {
+          avatar: 'user-avatar',
+          discriminator: '1234',
+          id: 'user-id',
+        },
+      })
+
+      expect(url).toContain('gif')
+    })
   })
 
   describe('getUserAvatarUrl', () => {
