4e0ae2da1351a15d4a5fe551b8db04371d5d7055
Felix <jonfelixrico@gmail.com>
2023-01-07 21:09:49 +0800

    Add decorator for server id key

---
+++ src/discord-api/decorators/discord-server-id-key.decorator.ts
@@ -0,0 +1,8 @@
+import { SetMetadata } from '@nestjs/common'
+import { SERVER_ID_PARAM_KEY } from '../guards/discord-server-access.guard'
+
+/**
+ * This will allow other decorators to know the param key used for specify a discord server id.
+ */
+export const DiscordServerIdKey = () =>
+  SetMetadata(SERVER_ID_PARAM_KEY, 'serverId')
