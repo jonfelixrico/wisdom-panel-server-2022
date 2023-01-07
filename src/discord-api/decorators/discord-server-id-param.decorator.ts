e72ba811ab679433fb1b720877dec22df88e21eb
Felix <jonfelixrico@gmail.com>
2023-01-07 21:10:18 +0800

    Use a better name

--- src/discord-api/decorators/discord-server-id-param.decorator.ts
+++ src/discord-api/decorators/discord-server-id-param.decorator.ts
@@ -4,5 +4,5 @@ import { SERVER_ID_PARAM_KEY } from '../guards/discord-server-access.guard'
 /**
  * This will allow other decorators to know the param key used for specify a discord server id.
  */
-export const DiscordServerIdKey = () =>
+export const DiscordServerIdParam = () =>
   SetMetadata(SERVER_ID_PARAM_KEY, 'serverId')
