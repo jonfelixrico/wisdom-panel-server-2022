648bdebce80b7d45a7b60a2815d839cdc3621183
Felix <jonfelixrico@gmail.com>
2023-01-07 16:10:06 +0800

    Add OpenAPI docu

--- src/discord-controllers/controllers/server-member/server-member.controller.ts
+++ src/discord-controllers/controllers/server-member/server-member.controller.ts
@@ -1,5 +1,11 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+} from '@nestjs/common'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { ServerMemberApiService } from 'src/discord-api/services/server-member-api/server-member-api.service'
 import { getMemberAvatarUrl } from 'src/discord-api/utils/avatar.utils'
 import { ServerMemberDto } from './server-member.dto'
@@ -9,6 +15,14 @@ import { ServerMemberDto } from './server-member.dto'
 export class ServerMemberController {
   constructor(private repo: ServerMemberApiService) {}
 
+  @ApiOperation({
+    operationId: 'getServerMember',
+    summary: "Get info about a server's member",
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'The server or the member was not found.',
+  })
   @Get(':userId')
   async getMember(
     @Param('userId') userId: string,
