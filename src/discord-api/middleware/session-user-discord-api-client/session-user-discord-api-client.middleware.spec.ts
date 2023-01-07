b5d851bb90a002e166af3c03a6dd7a24059dd335
Felix <jonfelixrico@gmail.com>
2023-01-07 17:04:50 +0800

    Use interceptors instead of middleware

--- src/discord-api/middleware/session-user-discord-api-client/session-user-discord-api-client.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SessionUserDiscordApiClientMiddleware } from './session-user-discord-api-client.middleware'
-
-describe('SessionUserDiscordApiClientMiddleware', () => {
-  it('should be defined', () => {
-    expect(new SessionUserDiscordApiClientMiddleware()).toBeDefined()
-  })
-})
