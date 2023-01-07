141ffa252789b909da432e149000cf439ad9de2d
Felix <jonfelixrico@gmail.com>
2023-01-07 17:55:41 +0800

    Add the session user interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { APP_GUARD } from '@nestjs/core'
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core'
 import { DiscordOauthModule } from './discord-oauth/discord-oauth.module'
 import { SessionGuard } from './guards/session.guard'
 import { SessionModule } from './session/session.module'
@@ -8,6 +8,7 @@ import { DiscordApiModule } from './discord-api/discord-api.module'
 import { WisdomApiModule } from './wisdom-api/wisdom-api.module'
 import { WisdomControllersModule } from './wisdom-controllers/wisdom-controllers.module'
 import { DiscordControllersModule } from './discord-controllers/discord-controllers.module'
+import { InjectSessionUserDiscordApiClientInterceptor } from './discord-api/interceptors/inject-session-user-discord-api-client/inject-session-user-discord-api-client.interceptor'
 
 @Module({
   imports: [
@@ -27,6 +28,10 @@ import { DiscordControllersModule } from './discord-controllers/discord-controll
     {
       provide: APP_GUARD,
       useClass: SessionGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: InjectSessionUserDiscordApiClientInterceptor,
     },
   ],
 })
