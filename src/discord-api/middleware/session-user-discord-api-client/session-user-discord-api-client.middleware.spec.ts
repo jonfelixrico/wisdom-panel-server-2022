c1dc91770a867cf9b1946a426f402ddfb0ea397f
Felix <jonfelixrico@gmail.com>
2023-01-07 17:00:49 +0800

    Generate the middleware

---
+++ src/discord-api/middleware/session-user-discord-api-client/session-user-discord-api-client.middleware.spec.ts
@@ -0,0 +1,7 @@
+import { SessionUserDiscordApiClientMiddleware } from './session-user-discord-api-client.middleware'
+
+describe('SessionUserDiscordApiClientMiddleware', () => {
+  it('should be defined', () => {
+    expect(new SessionUserDiscordApiClientMiddleware()).toBeDefined()
+  })
+})
