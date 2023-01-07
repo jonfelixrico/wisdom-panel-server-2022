3f80e6db93acd03c58f00d37b142f426d68bbaaf
Felix <jonfelixrico@gmail.com>
2023-01-07 16:04:55 +0800

    Add 404-wrapping to ServerMemberApiService

--- src/discord-api/services/server-member-api/server-member-api.service.ts
+++ src/discord-api/services/server-member-api/server-member-api.service.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { HttpStatus, Inject, Injectable } from '@nestjs/common'
 import { DiscordBotApiClient } from 'src/discord-api/providers/discord-bot-api.provider'
 import { DISCORD_BOT_CACHE } from 'src/discord-api/providers/discord-bot-cache.provider'
 import { Cache } from 'cache-manager'
 import { RESTGetAPIGuildMemberResult, Routes } from 'discord-api-types/v10'
+import { isAxiosError } from 'axios'
 
 @Injectable()
 export class ServerMemberApiService {
@@ -11,12 +12,23 @@ export class ServerMemberApiService {
     @Inject(DISCORD_BOT_CACHE) private cache: Cache,
   ) {}
 
-  async getMember(serverId: string, userId: string) {
+  async getMember(
+    serverId: string,
+    userId: string,
+  ): Promise<RESTGetAPIGuildMemberResult | null> {
     return this.cache.wrap(`server/${serverId}/user/${userId}`, async () => {
-      const { data } = await this.api.get<RESTGetAPIGuildMemberResult>(
-        Routes.guildMember(serverId, userId),
-      )
-      return data
+      try {
+        const { data } = await this.api.get<RESTGetAPIGuildMemberResult>(
+          Routes.guildMember(serverId, userId),
+        )
+        return data
+      } catch (e) {
+        if (isAxiosError(e) && e.status === HttpStatus.NOT_FOUND) {
+          return null
+        }
+
+        throw e
+      }
     })
   }
 }
