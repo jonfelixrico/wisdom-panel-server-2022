30bd89415553b0e44543c228015ffa2e86103633
Felix <jonfelixrico@gmail.com>
2023-01-07 16:07:01 +0800

    Make getMember throw an exception on null

--- src/discord-controllers/controllers/server-member/server-member.controller.ts
+++ src/discord-controllers/controllers/server-member/server-member.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { ServerMemberApiService } from 'src/discord-api/services/server-member-api/server-member-api.service'
 import { getMemberAvatarUrl } from 'src/discord-api/utils/avatar.utils'
@@ -15,6 +15,9 @@ export class ServerMemberController {
     @Param('serverId') serverId: string,
   ): Promise<ServerMemberDto> {
     const member = await this.repo.getMember(serverId, userId)
+    if (!member) {
+      throw new NotFoundException()
+    }
 
     return {
       username: member.nick || member.user.username,
