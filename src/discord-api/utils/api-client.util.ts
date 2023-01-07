e523d3a910f3cb0d9d446544585c0720972e6e6f
Felix <jonfelixrico@gmail.com>
2023-01-07 16:54:39 +0800

    Remove refresh-related code

--- src/discord-api/utils/api-client.util.ts
+++ src/discord-api/utils/api-client.util.ts
@@ -1,16 +1,5 @@
 import axios from 'axios'
-import {
-  OAuth2Routes,
-  RESTPostOAuth2RefreshTokenResult,
-  RESTPostOAuth2RefreshTokenURLEncodedData,
-  RouteBases,
-} from 'discord-api-types/v10'
-import { stringify } from 'qs'
-
-interface AppOAuth2ClientInformation {
-  clientId: string
-  clientSecret: string
-}
+import { RouteBases } from 'discord-api-types/v10'
 
 export function createClient(accessToken: string, tokenType: string) {
   return axios.create({
@@ -19,27 +8,4 @@ export function createClient(accessToken: string, tokenType: string) {
     },
     baseURL: RouteBases.api,
   })
-}
-
-export async function refreshToken(
-  refreshToken: string,
-  appClientInfo: AppOAuth2ClientInformation,
-) {
-  const refreshTokenData: RESTPostOAuth2RefreshTokenURLEncodedData = {
-    client_id: appClientInfo.clientId,
-    client_secret: appClientInfo.clientSecret,
-    grant_type: 'refresh_token',
-    refresh_token: refreshToken,
-  }
-
-  const { data } = await axios.post<RESTPostOAuth2RefreshTokenResult>(
-    OAuth2Routes.tokenURL,
-    stringify(refreshTokenData),
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    },
-  )
-  return data
 }
