e777e833d44effafa7ee2623c332a1cfddcb6e01
Felix <jonfelixrico@gmail.com>
2023-01-07 17:46:41 +0800

    Inject the axios instance into the request object

--- src/discord-api/interceptors/inject-session-user-discord-api-client/inject-session-user-discord-api-client.interceptor.ts
+++ src/discord-api/interceptors/inject-session-user-discord-api-client/inject-session-user-discord-api-client.interceptor.ts
@@ -4,13 +4,26 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common'
+import { Request } from 'express'
 import { Observable } from 'rxjs'
+import { createClient } from 'src/discord-api/utils/api-client.util'
 
 @Injectable()
 export class InjectSessionUserDiscordApiClientInterceptor
   implements NestInterceptor
 {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    if (context.getType() !== 'http') {
+      return next.handle()
+    }
+
+    const req = context.switchToHttp().getRequest<Request>()
+
+    // should always have a value since this should come after an auth guard
+    const { accessToken, tokenType } = req.session.credentials
+    // TODO impl refresh token
+    req.sessionUserDiscordApi = createClient(accessToken, tokenType)
+
     return next.handle()
   }
 }
