b91f3247e59ae9c5a606b3f3bb4ed101e9289838
Felix <jonfelixrico@gmail.com>
2023-01-07 22:20:36 +0800

    Use the discord server guard

--- src/wisdom-controllers/controllers/server-quote/server-quote.controller.ts
+++ src/wisdom-controllers/controllers/server-quote/server-quote.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  UseGuards,
+} from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { DiscordServerIdParam } from 'src/discord-api/decorators/discord-server-id-param.decorator'
+import { DiscordServerAccessGuard } from 'src/discord-api/guards/discord-server-access.guard'
 import { QuoteApiService } from 'src/wisdom-api/services/quote-api/quote-api.service'
 import { QuoteDto } from 'src/wisdom-controllers/dto/quote.dto'
 
+@UseGuards(DiscordServerAccessGuard)
 @ApiTags('wisdom')
 @Controller('server/:serverId/quote')
 export class ServerQuoteController {
@@ -16,6 +25,7 @@ export class ServerQuoteController {
     status: 404,
     description: 'Quote not found, or server not found.',
   })
+  @DiscordServerIdParam()
   @Get(':quoteId')
   async getQuote(
     // TODO make required
--- src/wisdom-controllers/wisdom-controllers.module.ts
+++ src/wisdom-controllers/wisdom-controllers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { DiscordApiModule } from 'src/discord-api/discord-api.module'
 import { WisdomApiModule } from 'src/wisdom-api/wisdom-api.module'
 import { ServerQuoteController } from './controllers/server-quote/server-quote.controller'
 
 @Module({
-  imports: [WisdomApiModule],
+  imports: [WisdomApiModule, DiscordApiModule],
   controllers: [ServerQuoteController],
 })
 export class WisdomControllersModule {}
