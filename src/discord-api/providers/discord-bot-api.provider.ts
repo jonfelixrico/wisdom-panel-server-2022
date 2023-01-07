b0ded0d7ef7774c717d8cb96955beea550e40d41
Felix <jonfelixrico@gmail.com>
2023-01-07 21:47:15 +0800

    Add interceptor to adjust the message code

--- src/discord-api/providers/discord-bot-api.provider.ts
+++ src/discord-api/providers/discord-bot-api.provider.ts
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { RouteBases } from 'discord-api-types/v10'
 
 import { Axios } from 'axios'
+import { isDiscordError } from '../utils/api-client.util'
 
 export class DiscordBotApiClient extends Axios {}
 
@@ -11,12 +12,25 @@ export function provideDiscordBotApiClient(): Provider {
   return {
     provide: DiscordBotApiClient,
     useFactory(cfg: ConfigService) {
-      return axios.create({
+      const instance = axios.create({
         baseURL: RouteBases.api,
         headers: {
           Authorization: `Bot ${cfg.getOrThrow('DISCORD_BOT_TOKEN')}`,
         },
       })
+
+      instance.interceptors.response.use(
+        (res) => res,
+        (err) => {
+          if (isDiscordError(err)) {
+            err.message = `${err.response.data.message} (${err.response.data.code})`
+          }
+
+          return Promise.reject(err)
+        },
+      )
+
+      return instance
     },
     inject: [ConfigService],
   }
