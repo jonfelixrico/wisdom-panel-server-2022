4f2e4d7c36abe8574ee28dbf640220c8e9adcfc3
Felix <jonfelixrico@gmail.com>
2023-01-10 00:33:26 +0800

    Include userId in the session

--- src/custom/express-session.d.ts
+++ src/custom/express-session.d.ts
@@ -3,5 +3,6 @@ import { DiscordUserOAuth2Credentials } from '../discord-oauth/types'
 declare module 'express-session' {
   interface SessionData {
     credentials: DiscordUserOAuth2Credentials
+    userId: string
   }
 }
