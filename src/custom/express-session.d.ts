457d327c2ed3459380776012cc29a5217d26729e
Felix <jonfelixrico@gmail.com>
2023-01-10 00:42:52 +0800

    Add comments

--- src/custom/express-session.d.ts
+++ src/custom/express-session.d.ts
@@ -2,6 +2,9 @@ import { DiscordUserOAuth2Credentials } from '../discord-oauth/types'
 
 declare module 'express-session' {
   interface SessionData {
+    /**
+     * The credentials of the session user. Can be used to do calls to the Discord API.
+     */
     credentials: DiscordUserOAuth2Credentials
     userId: string
   }
--- src/discord-api/interceptors/inject-session-user-discord-api-client/session-user-discord-api-client.class.ts
+++ src/discord-api/interceptors/inject-session-user-discord-api-client/session-user-discord-api-client.class.ts
@@ -1,5 +1,8 @@
 import { Axios } from 'axios'
 
 export abstract class SessionUserDiscordApiClient extends Axios {
+  /**
+   * The id of the Discord user which owns the credentials for this Axios instance.
+   */
   userId: string
 }
