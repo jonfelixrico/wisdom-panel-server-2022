d9175d0e64ce0fc4be1ed003e4a98c59f79d6227
Felix <jonfelixrico@gmail.com>
2023-01-10 00:50:55 +0800

    Adjust caching strings

--- src/discord-api/services/server-member-api/server-member-api.service.ts
+++ src/discord-api/services/server-member-api/server-member-api.service.ts
@@ -15,9 +15,10 @@ export class ServerMemberApiService {
 
   async isBotMemberOf(serverId: string): Promise<boolean> {
     // TODO move this to server API
-    return this.cache.wrap(`bot:server/${serverId}/user/@me`, async () => {
+    const url = Routes.guild(serverId)
+    return this.cache.wrap(url, async () => {
       try {
-        await this.api.get(Routes.guild(serverId))
+        await this.api.get(url)
         return true
       } catch (e) {
         if (isDiscordError(e) && e.response.status === HttpStatus.FORBIDDEN) {
@@ -33,52 +34,46 @@ export class ServerMemberApiService {
     serverId: string,
     userId: string,
   ): Promise<RESTGetAPIGuildMemberResult | null> {
-    return await this.cache.wrap(
-      `server/${serverId}/user/${userId}`,
-      async () => {
-        try {
-          const { data } = await this.api.get<RESTGetAPIGuildMemberResult>(
-            Routes.guildMember(serverId, userId),
-          )
-          return data
-        } catch (e) {
-          // handle user not being a member of the server
-          if (isDiscordError(e) && e.response.status === HttpStatus.NOT_FOUND) {
-            return null
-          }
-
-          /*
-           * TODO do the following tests to make the handling more robust
-           * - try to access server that that bot does not have access to
-           * - try to access a nonexistent server
-           * - try to access a nonexistent user
-           * - try to access a user which doesnt belong to the server
-           */
-
-          throw e
+    const url = Routes.guildMember(serverId, userId)
+    return await this.cache.wrap(url, async () => {
+      try {
+        const { data } = await this.api.get<RESTGetAPIGuildMemberResult>(url)
+        return data
+      } catch (e) {
+        // handle user not being a member of the server
+        if (isDiscordError(e) && e.response.status === HttpStatus.NOT_FOUND) {
+          return null
         }
-      },
-    )
+
+        /*
+         * TODO do the following tests to make the handling more robust
+         * - try to access server that that bot does not have access to
+         * - try to access a nonexistent server
+         * - try to access a nonexistent user
+         * - try to access a user which doesnt belong to the server
+         */
+
+        throw e
+      }
+    })
   }
 
   async isUserMemberOf(
     client: SessionUserDiscordApiClient,
     serverId: string,
   ): Promise<boolean> {
-    return this.cache.wrap(
-      `user@${client.userId}:server/${serverId}/user/@me`,
-      async () => {
-        try {
-          await client.get(Routes.userGuildMember(serverId))
-          return true
-        } catch (e) {
-          if (isDiscordError(e) && e.response.status === HttpStatus.FORBIDDEN) {
-            return false
-          }
-
-          throw e
+    const url = Routes.userGuildMember(serverId)
+    return this.cache.wrap(`${client.userId}:${url}`, async () => {
+      try {
+        await client.get(Routes.userGuildMember(serverId))
+        return true
+      } catch (e) {
+        if (isDiscordError(e) && e.response.status === HttpStatus.FORBIDDEN) {
+          return false
         }
-      },
-    )
+
+        throw e
+      }
+    })
   }
 }
