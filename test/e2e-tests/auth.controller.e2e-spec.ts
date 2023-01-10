0b2067e3ae1b79ec893dceba4fe5c686088e86db
Felix <jonfelixrico@gmail.com>
2023-01-11 01:04:11 +0800

    Add /callback with session test case

--- test/e2e-tests/auth.controller.e2e-spec.ts
+++ test/e2e-tests/auth.controller.e2e-spec.ts
@@ -6,12 +6,12 @@ import { mockExpressSession } from 'test/utils/mock-express-session'
 import { ConfigService } from '@nestjs/config'
 import { OAuthHelperService } from 'src/discord-oauth/services/oauth-helper/oauth-helper.service'
 
+const MOCK_AUTH_URL = 'http://authorization/'
+const MOCK_FE_URL = 'http://frontend/'
+
 describe('AuthController (e2e)', () => {
   describe('GET auth/oauth/discord', () => {
     let app: INestApplication
-
-    const MOCK_AUTH_URL = 'http://authorization/'
-    const MOCK_FE_URL = 'http://frontend/'
 
     beforeEach(async () => {
       const moduleFixture = await Test.createTestingModule({
@@ -63,5 +63,34 @@ describe('AuthController (e2e)', () => {
   describe('GET auth/oauth/discord/callback -- success cases', () => {
     it.todo('should redirect to FE with state')
     it.todo('should create a session')
+  })
+
+  describe('GET auth/oauth/discord/callback -- misc cases', () => {
+    let app: INestApplication
+
+    beforeEach(async () => {
+      const moduleFixture = await Test.createTestingModule({
+        imports: [AppModule],
+      })
+        .overrideProvider(OAuthHelperService)
+        .useValue({})
+        .overrideProvider(ConfigService)
+        .useValue({
+          getOrThrow: jest.fn().mockReturnValue(MOCK_FE_URL),
+        } as Pick<ConfigService, 'getOrThrow'>)
+        .compile()
+
+      app = moduleFixture.createNestApplication()
+    })
+
+    it('should redirect to FE if there is already a session', async () => {
+      mockExpressSession(app)
+      await app.init()
+
+      return request(app.getHttpServer())
+        .get('/auth/oauth/discord/callback')
+        .expect(HttpStatus.FOUND)
+        .expect('Location', MOCK_FE_URL)
+    })
   })
 })
