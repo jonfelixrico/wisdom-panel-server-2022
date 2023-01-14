81fb54393162686b59c4ccf9c01ac2947f448ed4
Felix <jonfelixrico@gmail.com>
2023-01-15 04:36:27 +0800

    Issue #51: Add strategies to prevent rate limit in smaller servers (#56)

--- src/discord-api/providers/discord-api-cache.provider.ts
+++ src/discord-api/providers/discord-api-cache.provider.ts
@@ -1,16 +1,37 @@
 import { Provider } from '@nestjs/common'
-import { caching } from 'cache-manager'
+import { caching, Cache } from 'cache-manager'
+import { PromiseCache } from 'src/utils/promise-cache.utils'
 
 export const DISCORD_API_CACHE = Symbol('discord api cache')
+
+export interface ApiCache extends Cache {
+  /**
+   * Similar to `wrap`, but this makes sure that there is only one instance of the wrapped
+   * function running at any given time within the Cache context.
+   */
+  wrapV2: Cache['wrap']
+}
 
 export function provideDiscordApiCache(): Provider {
   return {
     provide: DISCORD_API_CACHE,
-    useFactory: () => {
-      return caching('memory', {
+    useFactory: async () => {
+      const promiseCache = new PromiseCache()
+      const dataCache = await caching('memory', {
         max: 100,
         ttl: 60 * 1000,
       })
+
+      const wrapV2: Cache['wrap'] = (key, fn, ttl) => {
+        const promiseCached = () => promiseCache.run(key, fn)
+        return dataCache.wrap(key, promiseCached, ttl)
+      }
+
+      Object.assign(dataCache, {
+        wrapV2,
+      })
+
+      return dataCache
     },
   }
 }
