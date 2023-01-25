025b0b0dbf7e07a26c6124fbe6a0f47da6c5a559
Felix <jonfelixrico@gmail.com>
2023-01-26 01:16:51 +0800

    Use controller instead of service

--- src/discord-controllers/discord-controllers.module.ts
+++ src/discord-controllers/discord-controllers.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DiscordApiModule } from 'src/discord-api/discord-api.module'
 import { ServerMemberController } from './controllers/server-member/server-member.controller'
-import { ServerService } from './controllers/server/server.service'
+import { ServerController } from './controllers/server/server.controller'
 
 @Module({
   imports: [DiscordApiModule],
-  controllers: [ServerMemberController],
-  providers: [ServerService],
+  controllers: [ServerMemberController, ServerController],
 })
 export class DiscordControllersModule {}
