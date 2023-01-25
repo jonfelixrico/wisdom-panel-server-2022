824c8654b2b74181294d1f9104465649dd4bac6f
Felix <jonfelixrico@gmail.com>
2023-01-26 01:05:52 +0800

    Create getUserServers method

--- src/discord-api/services/server-api/server-api.service.ts
+++ src/discord-api/services/server-api/server-api.service.ts
@@ -1,21 +1,41 @@
-import { Injectable } from '@nestjs/common'
-import { RESTAPIPartialCurrentUserGuild } from 'discord-api-types/v10'
+import { Inject, Injectable } from '@nestjs/common'
+import { Cache } from 'cache-manager'
+import {
+  RESTAPIPartialCurrentUserGuild,
+  RESTGetAPICurrentUserGuildsResult,
+  Routes,
+} from 'discord-api-types/v10'
 import { orderBy } from 'lodash'
+import { DISCORD_API_CACHE } from 'src/discord-api/providers/discord-api-cache.provider'
+import { SessionUserClient } from 'src/discord-api/utils/api-client.util'
 import { BotServersCacheService } from '../bot-servers-cache/bot-servers-cache.service'
 
 @Injectable()
 export class ServerApiService {
-  constructor(private serverCache: BotServersCacheService) {}
+  constructor(
+    private servers: BotServersCacheService,
+    @Inject(DISCORD_API_CACHE) private cache: Cache,
+  ) {}
 
   async getServer(
     serverId: string,
   ): Promise<RESTAPIPartialCurrentUserGuild | null> {
-    const servers = await this.serverCache.getServers()
+    const servers = await this.servers.getServers()
     return servers[serverId] ?? null
   }
 
   async getServers(): Promise<RESTAPIPartialCurrentUserGuild[]> {
-    const servers = await this.serverCache.getServers()
+    const servers = await this.servers.getServers()
     return orderBy(Object.values(servers), ['name'], ['asc'])
+  }
+
+  async getUserServers(
+    client: SessionUserClient,
+  ): Promise<RESTGetAPICurrentUserGuildsResult> {
+    const url = Routes.userGuilds()
+    return this.cache.wrap(`${client.userId}:${url}`, async () => {
+      const { data } = await client.get<RESTGetAPICurrentUserGuildsResult>(url)
+      return data
+    })
   }
 }
