ced5befdf5e64466122a2d36848eff19efae1519
Felix <jonfelixrico@gmail.com>
2023-01-26 01:08:24 +0800

    Create controller

--- src/discord-controllers/discord-controllers.module.ts
+++ src/discord-controllers/discord-controllers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { DiscordApiModule } from 'src/discord-api/discord-api.module'
 import { ServerMemberController } from './controllers/server-member/server-member.controller'
+import { ServerService } from './controllers/server/server.service'
 
 @Module({
   imports: [DiscordApiModule],
   controllers: [ServerMemberController],
+  providers: [ServerService],
 })
 export class DiscordControllersModule {}
