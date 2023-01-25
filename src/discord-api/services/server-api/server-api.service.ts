5098f25df92d0f3992df36928879ad1554136fee
Felix <jonfelixrico@gmail.com>
2023-01-26 00:56:08 +0800

    Add server-related methods

--- src/discord-api/services/server-api/server-api.service.ts
+++ src/discord-api/services/server-api/server-api.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common'
+import { RESTAPIPartialCurrentUserGuild } from 'discord-api-types/v10'
+import { orderBy } from 'lodash'
+import { BotServersCacheService } from '../bot-servers-cache/bot-servers-cache.service'
 
 @Injectable()
-export class ServerApiService {}
+export class ServerApiService {
+  constructor(private serverCache: BotServersCacheService) {}
+
+  async getServer(
+    serverId: string,
+  ): Promise<RESTAPIPartialCurrentUserGuild | null> {
+    const servers = await this.serverCache.getServers()
+    return servers[serverId] ?? null
+  }
+
+  async getServers(): Promise<RESTAPIPartialCurrentUserGuild[]> {
+    const servers = await this.serverCache.getServers()
+    return orderBy(Object.values(servers), ['name'], ['asc'])
+  }
+}
