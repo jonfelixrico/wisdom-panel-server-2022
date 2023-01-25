7577dcf132f21dcf0c6cd17d7eba19a78935dcca
Felix <jonfelixrico@gmail.com>
2023-01-26 00:35:09 +0800

    Adjust the structure

--- src/discord-api/services/bot-servers-cache/bot-servers-cache.service.spec.ts
+++ src/discord-api/services/bot-servers-cache/bot-servers-cache.service.spec.ts
@@ -1,35 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing'
+import { Test } from '@nestjs/testing'
 import { createMock } from '@golevelup/ts-jest'
 import { BotServersCacheService } from './bot-servers-cache.service'
 import { DiscordBotApiClient } from 'src/discord-api/providers/discord-bot-api.provider'
 import { PromiseCache } from 'src/utils/promise-cache.utils'
 
 describe('BotServersCacheService', () => {
-  let service: BotServersCacheService
-  let module: TestingModule
-
-  beforeEach(async () => {
-    module = await Test.createTestingModule({
-      providers: [
-        BotServersCacheService,
-        {
-          provide: DiscordBotApiClient,
-          useValue: createMock<DiscordBotApiClient>(),
-        },
-        {
-          provide: PromiseCache,
-          useValue: createMock<PromiseCache>(),
-        },
-      ],
-    }).compile()
-
-    service = module.get(BotServersCacheService)
-  })
-
   describe('getServers', () => {
     let promiseCache: PromiseCache
+    let service: BotServersCacheService
 
-    beforeEach(() => {
+    beforeEach(async () => {
+      const module = await Test.createTestingModule({
+        providers: [
+          BotServersCacheService,
+          {
+            provide: DiscordBotApiClient,
+            useValue: createMock<DiscordBotApiClient>(),
+          },
+          {
+            provide: PromiseCache,
+            useValue: createMock<PromiseCache>(),
+          },
+        ],
+      }).compile()
+
+      service = module.get(BotServersCacheService)
       promiseCache = module.get(PromiseCache)
     })
 
