524d18f66c4d418740368ff529d457d7820c0466
Felix <jonfelixrico@gmail.com>
2023-02-01 23:10:56 +0800

    Adjust the DTO

--- src/wisdom-api/dto/quote.wisdom-dto.ts
+++ src/wisdom-api/dto/quote.wisdom-dto.ts
@@ -1,6 +1,16 @@
 export interface WisdomApiQuoteReceiveDto {
   id: string
+  timestamp: Date
   userId: string
+  serverId: string
+  channelId: string
+  messageId: string
+  isLegacy: boolean
+}
+
+export interface WisdomAPIStatusDeclaration {
+  status: string
+  timestamp: Date
 }
 
 export interface WisdomApiQuoteDto {
@@ -9,5 +19,14 @@ export interface WisdomApiQuoteDto {
   authorId: string
   submitterId: string
   submitDt: Date
+  expirationDt: Date
+  serverId: string
+  channelId: string
+  messageId: string
   receives: WisdomApiQuoteReceiveDto[]
+  statusDeclaration: WisdomAPIStatusDeclaration | null
+  votes: Record<string, Date>
+  requiredVoteCount: number
+  isLegacy: boolean
+  revision: number
 }
