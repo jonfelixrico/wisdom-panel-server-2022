d892b8ce957060abfddace888a7f95dd6d1d3862
Felix <jonfelixrico@gmail.com>
2023-11-19 03:40:16 +0800

    Relocate

--- src/system-parameters/system-parameters.module.ts
+++ src/system-parameters/system-parameters.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { SystemParametersController } from './system-parameters.controller'
+import { SystemParametersController } from './controllers/system-parameters.controller'
 
 @Module({
   controllers: [SystemParametersController],
